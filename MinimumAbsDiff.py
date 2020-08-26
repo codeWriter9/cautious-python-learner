97b7a3d346e0258e097535c85ecc7a41df7cc099
codeWriter9 <nerdsubhankar2006@gmail.com>
2020-08-26 15:56:18 +0000

    commented out the unnecessary println

--- MinimumAbsDiff.py
+++ MinimumAbsDiff.py
@@ -1,16 +1,18 @@
-import math
-import os
-import random
-import re
-import sys
+from math import fabs;
+from sys import maxsize;
 
 
 def minimumAbsoluteDifference(arr):
   arr = sorted(arr);
-  for x in range(1, len(arr) - 1):
-    print(" arr[ " + str(x-1) + "] = " + str(arr[x-1])); 
-    print(" arr[ " + str(x) + "] = " + str(arr[x])); 
-  return 0
+  min = maxsize;
+  for x in range(1, len(arr)):
+    #print(" arr[ " + str(x-1) + "] = " + str(arr[x-1])); 
+    #print(" arr[ " + str(x) + "] = " + str(arr[x]));
+    diff = int(fabs(arr[x] - arr[x - 1]));
+    #print(" diff " + str(diff));
+    if diff < min:
+      min = diff 
+  return min
   
   
 
